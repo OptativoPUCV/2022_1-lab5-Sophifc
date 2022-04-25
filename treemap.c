690180cc1ab5dca7f0c61c379e14cae437de04d5
Sophifc <>
2022-04-25 02:07:14 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -134,7 +134,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
    // TreeNode *parent = node;
     TreeNode *aux = node->right;
     aux = minimum(aux);
-    node->key = aux->key;
+    node->pair->key = aux->key;
     //node->value = aux->value;
    /* if(parent->right == aux){
       parent->right = NULL;
