ed2998a4f97e6df5668dd14399fc0b3b5bd476b3
Sophifc <>
2022-04-25 01:53:04 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,9 +37,12 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
-    return NULL;
+  
+  TreeMap *MapaO = (TreeMap*) malloc(sizeof(TreeMap*));
+  MapaO->lower_than = lower_than;
+  return MapaO;
+  //new->lower_than = lower_than;
+  //return NULL;
 }
 
 
@@ -50,7 +53,6 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
   }
   else{
     //buscar donde ubicar el nodo
-    
     TreeNode *nuevoNodo=createTreeNode(key,value);
   }
   
