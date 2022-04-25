0d7928c768889c9529c1dbffbd220661b3ec89e2
Sophifc <>
2022-04-25 02:00:38 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -38,9 +38,9 @@ TreeNode * createTreeNode(void* key, void * value) {
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
   
-  TreeMap *MapaO = (TreeMap*) malloc(sizeof(TreeMap*));
-  MapaO->lower_than = lower_than;
-  return MapaO;
+  TreeMap *auxMapa = (TreeMap*) malloc(sizeof(TreeMap*));
+  auxMapa->lower_than = lower_than;
+  return auxMapa;
   //new->lower_than = lower_than;
   //return NULL;
 }
